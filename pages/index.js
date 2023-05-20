432c386fb4610fae4f92a6030f5c790ac505acde
Dave <daveistdave@gmail.com>
2023-05-21 01:03:06 +0200

    sssssssss

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 // index.js
 import Link from "next/link";
-const ab = require('./background.js')
 
+//const l = require('../background.js')
 export default function Index() {
   return (
     <div>
